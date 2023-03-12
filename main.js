cc8dc655c74a476bdf006fd52111971bb2d35c07
Your Name <you@example.com>
2023-03-12 20:25:49 +0200

    Js Task

---
+++ main.js
@@ -0,0 +1,80 @@
+
+/////////////////////////// Task One ///////////////////////////
+
+// var First = prompt ("Write the first number?");
+// var Second = prompt ("Write the Second number?");
+// if (First > Second) {
+//     console.log(First)
+// } else {
+//     console.log(Second)
+// }
+
+
+
+
+
+
+/////////////////////////// Task Two ///////////////////////////
+
+// var num1 = prompt("Enter the first number: ");
+// var num2 = prompt("Enter the second number: ");
+// var num3 = prompt("Enter the third number: ");
+
+// var maxNum = Math.max(num1, num2, num3);
+// var minNum = Math.min(num1, num2, num3);
+
+// console.log("The maximum number is: " + maxNum);
+// console.log("The minimum number is: " + minNum);
+
+
+
+
+
+
+
+
+/////////////////////////// Task Three ///////////////////////////
+
+// var num1 = prompt("Enter the marks for num1: ");
+// var num2 = prompt("Enter the marks for num2: ");
+// var num3 = prompt("Enter the marks for num3: ");
+// var num4 = prompt("Enter the marks for num4: ");
+// var num5 = prompt("Enter the marks for num5: ");
+
+// var totalMarks = num1 + num2 + num3 + num4 + num5;
+// var averageMarks = totalMarks / 5;
+// var percentage = (totalMarks / 500) * 100;
+
+// console.log("Total Marks: " + totalMarks);
+// console.log("Average Marks: " + averageMarks);
+// console.log("Percentage: " + percentage + "%");
+
+
+
+
+
+
+
+
+/////////////////////////// Task Four ///////////////////////////
+
+// var Name = prompt("Write the Name?");
+// console.log(Name.charAt(0).toLowerCase() + Name.slice(1, Name.length).toUpperCase());
+
+
+
+
+
+
+
+
+
+/////////////////////////// Task Five ///////////////////////////
+
+// var num = prompt("Enter a number to calculate its factorial: ");
+// var factorial = 1;
+
+// for (var i = 1; i <= num; i++) {
+//   factorial = factorial * i;
+// }
+// console.log(factorial);
